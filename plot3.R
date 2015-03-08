4b2c0982493cf26dcf8ec4ece8c076ab27bed82e
beardface1 <beardfac3@gmail.com>
2015-03-09 00:03:15 +0200

    Final Assigment R files Commit

--- plot3.R
+++ plot3.R
@@ -4,21 +4,13 @@ df[,1] <- as.Date(df[,1], format = "%d/%m/%Y")  ##set 'date'into date format
 
 dfg <- df[df$Date >= "2007-02-01" & df$Date <= "2007-02-02",]   ##subset the data
 
-y <- weekdays(as.Date(dfg[,1],'%d-%m-%Y'))
+x <- as.POSIXct(paste(dfg$Date, dfg$Time), format="%F %H:%M:%S")   ##combine columns 1 and 2 to make datetime
 
-x <- as.POSIXct(paste(dfg$Date, dfg$Time), format="%F %H:%M:%S")   
-
-dfg[,1] <- x    ##make column 1 DateTIme
-
-dfg[,2] <- y    ##make column 2 weekdays
+dfg[,1] <- x    ##make column 1 datetime
 
 rm(x)   ##remove x
 
-rm(y)   ##remove y
-
-names(dfg)[names(dfg) == 'Date'] <- 'DateTime'   ##rename column 1
-
-names(dfg)[names(dfg) == 'Time'] <- 'Weekday'   ##rename column 2
+names(dfg)[names(dfg) == 'Date'] <- 'datetime'   ##rename column 1
 
 dfg[,3] <- suppressWarnings(as.numeric(dfg[,3]))
 dfg[,4] <- suppressWarnings(as.numeric(dfg[,4]))
@@ -28,8 +20,12 @@ dfg[,7] <- suppressWarnings(as.numeric(dfg[,7]))
 dfg[,8] <- suppressWarnings(as.numeric(dfg[,8]))
 dfg[,9] <- suppressWarnings(as.numeric(dfg[,9]))  ##make other variables numeric
 
-plot(dfg$DateTime, dfg$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+png(file = "plot3.png", width = 480, height = 480)
 
-png(file = "plot2.png", width = 480, height = 480)
-with(dfg, plot(dfg$DateTime, dfg$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+plot(dfg$datetime, dfg$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+lines(dfg$datetime, dfg$Sub_metering_1, type='l')
+lines(dfg$datetime, dfg$Sub_metering_2, type='l', col='red')
+lines(dfg$datetime, dfg$Sub_metering_3, type='l', col='blue')
+legend("topright", col = c("black","blue", "red"), lty = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.off()  ##make plot png
